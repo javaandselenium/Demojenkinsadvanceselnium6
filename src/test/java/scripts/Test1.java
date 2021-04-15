62e664e9c9d51a8b5a06580b71ea947f16371515
QSP <QSP@QSP>
2021-04-15 19:56:58 +0530

    hoo

--- src/test/java/scripts/Test1.java
+++ src/test/java/scripts/Test1.java
@@ -3,14 +3,12 @@ package scripts;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class Test0 {
+public class Test1 {
 	@Test
 	public void demo() {
 		Reporter.log("hello java",true);
 		Reporter.log("hello selenium",true);
 		Reporter.log("hello python",true);
-		Reporter.log("hello appium",true);
-		
 	}
 
 	
