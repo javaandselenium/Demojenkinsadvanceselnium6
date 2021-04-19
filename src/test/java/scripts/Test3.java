c4cacf439a87c5ab8416fe0258c73c395b152f7c
QSP <QSP@QSP>
2021-04-19 18:54:29 +0530

    Hi

---
+++ src/test/java/scripts/Test3.java
@@ -0,0 +1,15 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test3 {
+	@Test
+	public void demo() {
+		Reporter.log("hello api",true);
+		
+		
+	}
+
+	
+}
